cc671426b41751214038eecdf407cfb319a5393a
Vika <vikapetrova8@gmail.com>
2025-10-03 00:12:03 +0300

    feat: add playlistsApi with get query + add types

---
+++ src/features/playlists/api/playlistsApi.ts
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type {
+  FetchPlaylistsArgs,
+  PlaylistsResponse,
+} from "@/features/playlists/api/playlistsApi.types.ts";
+
+export const playlistsApi = createApi({
+  reducerPath: "playlistsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    headers: {
+      "API-KEY": import.meta.env.VITE_API_KEY,
+    },
+  }),
+  endpoints: (build) => ({
+    fetchPlaylists: build.query<PlaylistsResponse, FetchPlaylistsArgs>({
+      query: () => {
+        return {
+          method: "get",
+          url: "playlists",
+        };
+      },
+    }),
+  }),
+});
