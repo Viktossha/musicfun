9bcefb01225898168b252abaf0486fca10b58893
Vika <vikapetrova8@gmail.com>
2025-10-04 00:35:36 +0300

    refactor: decompose PlaylistsPage

--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -1,24 +1,26 @@
 import {
   useDeletePlaylistMutation,
   useFetchPlaylistsQuery,
-  useUpdatePlaylistMutation,
 } from "@/features/playlists/api/playlistsApi.ts";
 import s from "./PlaylistsPage.module.css";
 import { CreatePlaylistForm } from "@/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx";
 import { useState } from "react";
-import { type SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import type {
   PlaylistData,
   UpdatePlaylistArgs,
 } from "@/features/playlists/api/playlistsApi.types.ts";
+import { PlaylistItem } from "@/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx";
+import { EditPlaylistForm } from "@/features/playlists/ui/PlaylistsPage/EditPlaylistForm/EditPlaylistForm.tsx";
 
 export const PlaylistsPage = () => {
   const [playlistId, setPlaylistId] = useState<string | null>(null);
+
   const { register, handleSubmit, reset } = useForm<UpdatePlaylistArgs>();
 
   const { data } = useFetchPlaylistsQuery();
+
   const [deletePlaylist] = useDeletePlaylistMutation();
-  const [updatePlaylist] = useUpdatePlaylistMutation();
 
   const deletePlaylistHandler = (playlistId: string) => {
     if (confirm("Are you sure you want to delete the playlist?")) {
@@ -39,13 +41,6 @@ export const PlaylistsPage = () => {
     }
   };
 
-  const onSubmit: SubmitHandler<UpdatePlaylistArgs> = (data) => {
-    if (!playlistId) return;
-    updatePlaylist({ playlistId, body: data }).then(() => {
-      setPlaylistId(null);
-    });
-  };
-
   return (
     <div className={s.container}>
       <h1>Playlists page</h1>
@@ -57,37 +52,19 @@ export const PlaylistsPage = () => {
           return (
             <div className={s.item} key={playlist.id}>
               {isEditing ? (
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <h2>Edit playlist</h2>
-                  <div>
-                    <input {...register("title")} placeholder={"title"} />
-                  </div>
-                  <div>
-                    <input
-                      {...register("description")}
-                      placeholder={"description"}
-                    />
-                  </div>
-                  <button type={"submit"}>save</button>
-                  <button
-                    type={"button"}
-                    onClick={() => editPlaylistHandler(null)}
-                  >
-                    cancel
-                  </button>
-                </form>
+                <EditPlaylistForm
+                  playlistId={playlistId}
+                  handleSubmit={handleSubmit}
+                  register={register}
+                  editPlaylist={editPlaylistHandler}
+                  setPlaylistId={setPlaylistId}
+                />
               ) : (
-                <div>
-                  <div>title: {playlist.attributes.title}</div>
-                  <div>description: {playlist.attributes.description}</div>
-                  <div>userName: {playlist.attributes.user.name}</div>
-                  <button onClick={() => deletePlaylistHandler(playlist.id)}>
-                    delete
-                  </button>
-                  <button onClick={() => editPlaylistHandler(playlist)}>
-                    update
-                  </button>
-                </div>
+                <PlaylistItem
+                  playlist={playlist}
+                  deletePlaylist={deletePlaylistHandler}
+                  editPlaylist={editPlaylistHandler}
+                />
               )}
             </div>
           );
